1c5526d208d32532cea6cc4ca3ba5aa7c9f012cc
AinorRikar <AinorRikar@users.noreply.github.com>
2021-05-07 21:29:03 +0300

    Initial commit

---
+++ modules/navbar-top.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" href="/"><span class="title">Info Lab</span></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item" data-placement="bottom" data-html="true" data-toggle="tooltip" title="Главная страница">
+                <a class="nav-link btn bold" type="button" href="/">Главная</a>
+            </li>
+            <li class="nav-item" data-placement="bottom" data-html="true" data-toggle="tooltip" title="Личный кабинет</br>пользователя">
+                <a class="nav-link btn bold" type="button" href="?lk">Личный кабинет</a>
+            </li>
+        </ul>
+    </div>
+</nav>
